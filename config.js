bf540fceadedf4314b46ce998e46b22317c79814
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-11 18:56:53 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * 共通設定
  */
 window.DPRO_TAKEOUT_CONFIG = Object.freeze({
-  VERSION: "TAKEOUT-7-PRODUCT-PHOTO-MANAGEMENT-20260711",
+  VERSION: "TAKEOUT-7-R1-PRODUCT-STOCK-NULL-FIX-20260711",
   SHOP_CODE: "takeout_demo",
   API_BASE_URL: "https://dpro-takeout-line-api.dpromstk2000.workers.dev",
   ADMIN_KEY: "1234",
