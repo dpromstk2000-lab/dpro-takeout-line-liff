bb3fc5a41f67fb04aca5ec14e81f7e8571ace5d2
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-11 17:08:33 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /**
  * DPRO テイクアウト・弁当予約 LINE
- * STEP TAKEOUT-5
+ * STEP TAKEOUT-6
  * 共通設定
  */
 window.DPRO_TAKEOUT_CONFIG = Object.freeze({
-  VERSION: "TAKEOUT-5-KITCHEN-IPAD-20260711",
+  VERSION: "TAKEOUT-6-MEMBER-CHANGE-CANCEL-REORDER-20260711",
   SHOP_CODE: "takeout_demo",
   API_BASE_URL: "https://dpro-takeout-line-api.dpromstk2000.workers.dev",
   ADMIN_KEY: "1234",
